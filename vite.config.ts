a38d3785915c052e20916012a6b7e0191a780546
orhan.shabanov@internet.ru <orhan.shabanov@internet.ru>
2025-11-11 14:45:56 -0800

    update vite config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,12 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/allset-la-web/",        // путь для GitHub Pages
+  build: {
+    outDir: "docs",               // билд сразу в папку docs
+  },
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
